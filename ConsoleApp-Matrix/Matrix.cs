508c2acd140b9590a5bd756fc3e288143c171c06
JakubOlejnik <jakubekolejnik@wp.pl>
2024-04-03 16:44:34 +0200

    Szkielet Macierzy, początek Thread

---
+++ ConsoleApp-Matrix/Matrix.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Matrix
+{
+    internal class Matrix
+    {
+        public int n {  get; set; } //rozmiar macierzy
+        public int[,] matrix { get; set; } //macierz 
+        public Matrix(int _n) {
+            n = _n;
+            matrix = new int[n, n];
+        }
+
+        public void RandomMatrix(){
+            var rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rand.Next(10);
+                }
+            }
+        }
+
+        public static Matrix operator *(Matrix a, Matrix b){
+            Matrix c = new Matrix(a.n);
+
+            for (int i = 0; i < a.n; i++)
+            {
+                for (int k = 0; k < a.n; k++)
+                {
+                    for (int j = 0; j < a.n; j++)
+                    {
+                        c.matrix[i, k] += a.matrix[i, j] * b.matrix[j, k];
+                    }
+                }
+            }
+            return c;
+        }
+
+        public override string ToString(){
+            string s = "";
+
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < n; j++){
+                    s += $"{matrix[i, j]} ";
+                }
+                s += "\n";
+            }
+            return s;
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp-Matrix/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApp_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 0, threads_number = 0;
+
+            Console.WriteLine("Wprowadź rozmiar macierzy: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wprowadź liczbę wątków: ");
+            threads_number = int.Parse(Console.ReadLine());
+
+
+            Thread[] threads = new Thread[n];
+            for (int i = 0; i < threads_number; i++)
+            {
+                threads[i] = new Thread(Welcome);
+                threads[i].Name = String.Format(" Thread : {0}", i + 1);
+            }
+            foreach (Thread x in threads)
+                x.Start();
+            Console.WriteLine(" Main : Hello !");
+
+
+            Matrix a = new Matrix(n);
+            Matrix b = new Matrix(n);
+            Matrix c = new Matrix(n);
+
+            a.RandomMatrix();
+            b.RandomMatrix();
+
+            Console.WriteLine("Macierz A:");
+            Console.WriteLine(a);
+
+            Console.WriteLine("Macierz B:");
+            Console.WriteLine(b);
+
+            DateTime startTime = DateTime.Now;
+            c = a * b;
+            DateTime stopTime = DateTime.Now;
+            TimeSpan totalTime = stopTime - startTime;
+
+            Console.WriteLine("Macierz A*B:");
+            Console.WriteLine(c);
+
+            Console.WriteLine($"Czas wykonania mnożenia macierzy o rozmiarze {n} wyniósł: {totalTime.TotalSeconds} s");
+        }
+    }
+}
