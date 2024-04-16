f10421eb7f20c9e1f61eb26984c8907340bffd70
JakubOlejnik <jakubekolejnik@wp.pl>
2024-04-16 04:18:20 +0200

    Do momentu Threads

---
+++ ConsoleApp-Matrix/Multi.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp_Matrix
+{
+    internal class Multi
+    {
+        public int threads_number { get; set; }
+        public int start { get; set; }
+        public int fields { get; set; }
+        public Matrix a;
+        public Matrix b;
+        public Matrix c;
+
+        public Multi(int number, Matrix _a, Matrix _b, Matrix _c, int _start, int _fields)
+        {
+            threads_number = number;
+            a = _a;
+            b = _b;
+            c = _c;
+            start = _start;
+            fields = _fields;
+        }
+
+        public void Welcome()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} : Hello !! fields={fields}, start={start}");
+        }
+        public void Multiplication()
+        {
+            int row = 0;
+            int col = 0;
+
+            for (int i = 0; i < fields; i++)
+            {
+                row = (start + i) / a.n;
+                col = (start + i) % a.n;
+                for (int j = 0; j < a.n; j++)
+                {
+                    c.matrix[row, col] += a.matrix[row, j] * b.matrix[j, col];
+                }
+            }
+
+
+        }
+    }
+}
--- ConsoleApp-Matrix/Program.cs
+++ ConsoleApp-Matrix/Program.cs
@@ -10,20 +10,9 @@
             Console.WriteLine("Wprowadź rozmiar macierzy: ");
             n = int.Parse(Console.ReadLine());
 
+            Console.WriteLine($"Twój komputer posiada {Environment.ProcessorCount} rdzeni.");
             Console.WriteLine("Wprowadź liczbę wątków: ");
             threads_number = int.Parse(Console.ReadLine());
-
-
-            Thread[] threads = new Thread[n];
-            for (int i = 0; i < threads_number; i++)
-            {
-                threads[i] = new Thread(Welcome);
-                threads[i].Name = String.Format(" Thread : {0}", i + 1);
-            }
-            foreach (Thread x in threads)
-                x.Start();
-            Console.WriteLine(" Main : Hello !");
-
 
             Matrix a = new Matrix(n);
             Matrix b = new Matrix(n);
@@ -32,21 +21,69 @@
             a.RandomMatrix();
             b.RandomMatrix();
 
+
+            int[] threads_fields = new int[threads_number];
+            for (int i = 0; i < threads_number; i++)
+            {
+                threads_fields[i] = n * n / threads_number;
+            }
+            if (((n * n) % threads_number) > 0)
+            {
+                for (int i = 0; i < ((n * n) % threads_number); i++)
+                {
+                    threads_fields[i]++;
+                }
+            }
+
+            int tmp = 0;
+            Multi[] tabMulti = new Multi[threads_number];
+            for (int i = 0; i < threads_number; i++)
+            {
+                tabMulti[i] = new Multi(threads_number, a, b, c, tmp, threads_fields[i]);
+                tmp += threads_fields[i];
+                tabMulti[i].c = c;
+            }
+
+           
+            Thread[] threads = new Thread[threads_number];
+
+            for (int i = 0; i < threads_number; i++)
+            {
+                threads[i] = new Thread(tabMulti[i].Multiplication);
+
+                threads[i].Name = i.ToString();
+            }
+            DateTime startTime1 = DateTime.Now;
+            foreach (Thread x in threads) x.Start();
+            foreach (Thread x in threads) x.Join();
+
+            DateTime stopTime1 = DateTime.Now;
+            TimeSpan totalTime1 = stopTime1 - startTime1;
+
+
+
+
             Console.WriteLine("Macierz A:");
             Console.WriteLine(a);
 
             Console.WriteLine("Macierz B:");
             Console.WriteLine(b);
 
-            DateTime startTime = DateTime.Now;
-            c = a * b;
-            DateTime stopTime = DateTime.Now;
-            TimeSpan totalTime = stopTime - startTime;
+            Console.WriteLine("Macierz C - Threads:");
+            Console.WriteLine(c);
 
+             DateTime startTime = DateTime.Now;
+             c = a * b;
+             DateTime stopTime = DateTime.Now;
+             TimeSpan totalTime = stopTime - startTime;
+            
             Console.WriteLine("Macierz A*B:");
             Console.WriteLine(c);
 
-            Console.WriteLine($"Czas wykonania mnożenia macierzy o rozmiarze {n} wyniósł: {totalTime.TotalSeconds} s");
+            Console.WriteLine($"Czas wykonania mnożenia macierzy o rozmiarze {n} wyniósł: {totalTime.TotalSeconds} s - Threads: {totalTime1.TotalSeconds} s");
         }
+
+
+
     }
 }
